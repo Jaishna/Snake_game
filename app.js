81267b7acc69760a2e8eedb552ac9bc05ba26f9e
Jaishna <jaishna@Jaishnas-MacBook-Air.local>
2020-10-21 23:46:47 +0530

    simple edit

--- app.js
+++ app.js
@@ -3,17 +3,5 @@ document.addEventListener("DOMContentLoaded", function () {
   newVal = document.createElement("p");
   newVal.innerHTML = '';
   pTag.appendChild(newVal);
-});
-// Async Tests Example
-/*
-window.onModulesLoaded = new Promise( function( resolve, reject ) {
-  setTimeout(function() {
-    pTag = document.querySelector("div");
-    pTag.innerHTML = '';
-    newVal = document.createElement("p");
-    newVal.innerHTML = 'Hello World';
-    pTag.appendChild(newVal);
-    resolve();
-  }, 100)
-});
-*/
+}); 
+
